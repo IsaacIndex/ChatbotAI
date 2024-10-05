955b0bcb9e57bacb3feb0e33ae32ffbf1ab2c376
IsaacIndex <isaacwongnh@gmail.com>
2024-10-06 03:13:18 +0800

    add crawl categories

--- crawl_url.py
+++ crawl_url.py
@@ -7,39 +7,60 @@ from langchain_core.documents import Document
 url = 'https://www.ishtar-collective.net'
 
 
-def crawl_books():
-    req = Request(f'{url}/categories/book-unveiling',
+def crawl_categories():
+    req = Request(f'{url}/categories',
                   headers={'User-Agent': 'Mozilla/5.0'})
     html_page = urlopen(req).read()
 
     soup = BeautifulSoup(html_page, 'html.parser')
-    div = soup.findAll('div', {'class': 'entry-thumbnail col-4'})
+    div = soup.findAll('div', {'class': 'category-list'})
+    categories = []
+    for category in div:
+        links = category.findAll('a')
+        for link in links:
+            categories.append(link.get('href'))
+    # print(categories)
+    return categories
 
-    links = []
-    for ele in div:
-        links.append(ele.find('a').get('href'))
 
-    # print(links)
-
+def crawl_books():
     docs = []
-
-    for link in links:
-        req = Request(url + link, headers={'User-Agent': 'Mozilla/5.0'})
+    categories = crawl_categories()
+    for category in categories:
+        req = Request(f'{url}{category}',
+                      headers={'User-Agent': 'Mozilla/5.0'})
         html_page = urlopen(req).read()
 
         soup = BeautifulSoup(html_page, 'html.parser')
-        descriptions = soup.find('div', {'class': 'description highlightable'})
-        # print(descriptions)
+        div = soup.findAll('div', {'class': 'entry-thumbnail col-4'})
 
-        paragraphs = descriptions.findAll('p')
+        links = []
+        for ele in div:
+            links.append(ele.find('a').get('href'))
 
-        metadata = {
-            "source": re.search(r"\/entries\/([A-Za-z0-9\S]+)#book-unveiling", link).group(1)
-        }
+        for link in links:
+            req = Request(url + link, headers={'User-Agent': 'Mozilla/5.0'})
+            html_page = urlopen(req).read()
 
-        for paragraph in paragraphs:
-            docs.append(
-                Document(page_content=paragraph.text, metadata=metadata))
+            soup = BeautifulSoup(html_page, 'html.parser')
+            descriptions = soup.find(
+                'div', {'class': 'description highlightable'})
 
-    # print(docs)
+            paragraphs = descriptions.findAll('p')
+
+            category_regex = re.search(r"/categories/(.*)$", category).group(1)
+            regex = rf"\/entries\/([A-Za-z0-9\S]+)#{category_regex}"
+            metadata = {
+                "source": re.search(regex, link).group(1)
+            }
+
+            for paragraph in paragraphs:
+                print(Document(page_content=paragraph.text, metadata=metadata))
+                docs.append(
+                    Document(page_content=paragraph.text, metadata=metadata))
+
     return docs
+
+
+if __name__ == "__main__":
+    crawl_books()
